aea4a028a8e1ef072b2e118c094d351b91e7fe18
20737671 <102918623+20737671@users.noreply.github.com>
2022-10-30 23:46:04 +0200

    Homework 06
    My homework 06

---
+++ Homework 06/Controllers/productsController.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework_06.Models;
+using PagedList.Mvc;
+using PagedList;
+
+
+
+namespace Homework_06.Controllers
+{
+    public class productsController : Controller
+    {
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+        // GET: products
+        public ActionResult Index(string SearchString, int? i)
+        {
+
+
+            SearchString = (SearchString == null) ? "" : SearchString;
+
+            ViewData["CurrentFilter"] = SearchString;
+
+            var products = db.products.Include(p => p.brand).Include(p => p.category).OrderByDescending(p => p.product_name.Contains(SearchString));
+
+
+
+            return View(products.ToList().ToPagedList(i ?? 1, 10));
+            //return View(products.ToList());
+
+        }
+
+
+
+
+
+
+        // GET: products/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    product product = db.products.Find(id);
+        //    if (product == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(product);
+        //}
+
+        // GET: products/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name");
+        //    ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name");
+        //    return View();
+        //}
+
+        // POST: products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "product_id,product_name,brand_id,category_id,model_year,list_price")] product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.products.Add(product);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
+        //    ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
+        //    return View(product);
+        //}
+
+        // GET: products/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    product product = db.products.Find(id);
+        //    if (product == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
+        //    ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
+        //    return View(product);
+        //}
+
+        // POST: products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "product_id,product_name,brand_id,category_id,model_year,list_price")] product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(product).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
+        //    ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
+        //    return View(product);
+        //}
+
+        // GET: products/Delete/5
+        //public ActionResult Delete(int? id)
+        ////{
+        ////    if (id == null)
+        ////    {
+        ////        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        ////    }
+        ////    product product = db.products.Find(id);
+        ////    if (product == null)
+        ////    {
+        ////        return HttpNotFound();
+        ////    }
+        ////    return View(product);
+        ////}
+
+        //// POST: products/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    product product = db.products.Find(id);
+        //    db.products.Remove(product);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+
+
+        public ActionResult Details(int? id)
+        {
+
+            if (Request.IsAjaxRequest())
+            {
+                product product = db.products.Find(id);
+                if (product != null)
+                {
+                    return PartialView(product);
+                }
+                else
+                {
+                    Response.StatusCode = 403;
+                    return PartialView("Error");
+                }
+            }
+            Response.StatusCode = 500;
+            return PartialView("Error");
+
+
+        }
+
+
+
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///
+
+
+
+
+
+        private BikeStoresEntities _context;
+        public productsController()
+        {
+            _context = new BikeStoresEntities();
+        }
+        
+        public JsonResult List()
+        {
+            return Json(_context.Product.ToList(), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Add(product user)
+        {
+            _context.Product.Add(user);
+            _context.SaveChanges();
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetbyID(int ID)
+        {
+            return Json(_context.Product.FirstOrDefault(x => x.product_id == ID), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(product user)
+        {
+            var data = _context.Product.FirstOrDefault(x => x.product_id == user.product_id);
+            if (data != null)
+            {
+                data.product_name = user.product_name;
+                data.model_year = user.model_year;
+                data.list_price = user.list_price;
+                data.brand = user.brand;
+                data.category = user.category;
+                _context.SaveChanges();
+            }
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int ID)
+        {
+            var data = _context.Product.FirstOrDefault(x => x.product_id == ID);
+            _context.Product.Remove(data);
+            _context.SaveChanges();
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+    }
+}
---
+++ Homework 06/Scripts/Product.js
@@ -0,0 +1,193 @@
+﻿$(document).ready(function () {
+    loadData();
+});
+function loadData() {
+    $.ajax({
+        url: "/Home/List",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.product_name + '</td>';
+                html += '<td>' + item.model_year + '</td>';
+                html += '<td>' + item.list_price + '</td>';
+                html += '<td>' + item.brand + '</td>';
+                html += '<td>' + item.category + '</td>';
+                html += '<td><a href="#" onclick="return getbyID(' + item.product_id + ')">Edit</a> | <a href="#" onclick="Delele(' + item.product_id + ')">Delete</a></td>';
+                html += '</tr>';
+            });
+            $('.tbody').html(html);
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+function Add() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var empObj = {
+        
+        Name: $('#Name').val(),
+        Age: $('#Year').val(),
+        State: $('#Price').val(),
+        Country: $('#Brand').val(),
+        Category: $('#Category').val()
+
+    };
+    $.ajax({
+        url: "/Home/Add",
+        data: JSON.stringify(empObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#myModal').modal('hide');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+function getbyID(Id) {
+    $('#Name').css('border-color', 'lightgrey');
+    $('#Year').css('border-color', 'lightgrey');
+    $('#Price').css('border-color', 'lightgrey');
+    $('#Brand').css('border-color', 'lightgrey');
+    $('#Category').css('border-color', 'lightgrey');
+
+    $.ajax({
+        url: "/Home/getbyID/" + Id,
+        typr: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            $('#Name').val(result.Name);
+            $('#Year').val(result.model_year);
+            $('#Price').val(result.list_price);
+            $('#Brand').val(result.brand);
+            $('#Category').val(result.category);
+            $('#myModal').modal('show');
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
+function Update() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var empObj = {
+        
+        Name: $('#Name').val(),
+        Year: $('#Year').val(),
+        Price: $('#Price').val(),
+        Brand: $('#Brand').val(),
+        Category: $('#Category').val(),
+
+    };
+    $.ajax({
+        url: "/Home/Update",
+        data: JSON.stringify(empObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#myModal').modal('hide');
+           
+            $('#Name').val("");
+            $('#Year').val("");
+            $('#Price').val("");
+            $('#Brand').val("");
+            $('#Category').val("");
+
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+function Delele(ID) {
+    var ans = confirm("Are you sure you want to delete this Record?");
+    if (ans) {
+        $.ajax({
+            url: "/Home/Delete/" + ID,
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+                loadData();
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+}
+function clearTextBox() {
+    $('#Name').val("");
+    $('#Year').val("");
+    $('#Price').val("");
+    $('#Brand').val("");
+    $('#Category').val("");
+
+    $('#btnUpdate').hide();
+    $('#btnAdd').show();
+    $('#Name').css('border-color', 'lightgrey');
+    $('#Year').css('border-color', 'lightgrey');
+    $('#Price').css('border-color', 'lightgrey');
+    $('#Brand').css('border-color', 'lightgrey');
+    $('#Category').css('border-color', 'lightgrey');
+
+}
+function validate() {
+    var isValid = true;
+    if ($('#Name').val().trim() == "") {
+        $('#Name').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Name').css('border-color', 'lightgrey');
+    }
+    if ($('#Year').val().trim() == "") {
+        $('#Year').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Year').css('border-color', 'lightgrey');
+    }
+    if ($('#Price').val().trim() == "") {
+        $('#Price').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Price').css('border-color', 'lightgrey');
+    }
+    if ($('#Brand').val().trim() == "") {
+        $('#Brand').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Brand').css('border-color', 'lightgrey');
+    }
+    if ($('#Category').val().trim() == "") {
+        $('#Category').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Category').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
